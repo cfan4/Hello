a034a31fbb8bc9635020043e6572c47298d0fe21
cfan4 <46791436+cfan4@users.noreply.github.com>
2019-01-17 11:17:12 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Chris Fan Justin Ernst
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chris Fan Justin Ernst\n");
+
+  return 0;
+}
